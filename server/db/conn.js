76096a94b367606af6b35d44a41960eb8204c429
Sahil Sidana <sahilsidana2002@gmail.com>
2024-04-04 19:29:57 +0530

    commit

---
+++ server/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const DB = process.env.URI;
+
+mongoose.connect(DB,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+})
+.then(()=>console.log("database connected")).
+catch((err)=>console.log("errr",err))
